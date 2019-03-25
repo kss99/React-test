7d81088c045197712f2e8eff0fea9fc1ccf20abd
username <user@eamil.mail>
2019-03-25 09:36:38 +0900

    React

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customer ={
+  'name':'홍길동',
+  'birthday':'821023',
+  'gender' :'남자',
+  'job':'학생'
+
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App-test">
-      <img src={logo} lat="logo"></img>
-      <h2>Let's develop management system!!</h2>
+      <Customer
       
-      </div>
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      
+      />
     );
   }
 }
